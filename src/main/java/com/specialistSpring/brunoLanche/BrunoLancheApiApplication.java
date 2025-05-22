4035272d4ee22167b4ac39e8a6c496732ae2dd26
PAULO VINICIUS SILVA COSTA <paulo.costa@mv.com.br>
2025-05-22 01:41:01 -0300

    Createning struct this project

---
+++ src/main/java/com/specialistSpring/brunoLanche/BrunoLancheApiApplication.java
@@ -0,0 +1,13 @@
+package com.specialistSpring.brunoLanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunoLancheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunoLancheApiApplication.class, args);
+	}
+
+}
